ed4ec958bcc566d8131437678df18578db57abbb
Anton Ryabchikov <ar@friendwork.ru>
2018-12-23 14:07:30 +0300

    Добавил тесты

---
+++ DokiAPI.Tests/PagesController_Test.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DokiAPI.Tests
+{
+    class PagesController_Test
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void PagesTest()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void OnePageTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
